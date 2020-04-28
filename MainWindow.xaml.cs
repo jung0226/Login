af669201ac80eb91c59acc9c4419f639c2953685
jung0226 <jsheemang25@naver.com>
2020-04-28 21:01:12 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_email.Text == "a" && txt_PW.Password == "0")
+                MessageBox.Show("로그인 되었습니다.");
+            else
+                MessageBox.Show("로그인에 실패했습니다.");
+        }
+    }
+}
